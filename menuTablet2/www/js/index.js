bd189795a6408fd251331d31f59af589144057ba
Carlos Scovino <cscovino@users.noreply.github.com>
2017-10-10 09:25:29 -0400

    update

--- menuTablet2/www/js/index.js
+++ menuTablet2/www/js/index.js
@@ -1152,10 +1152,18 @@ var app = {
             for (var i=0; i<app.order.length; i++) {
                 aux.push(app.order[i]);
             }
-            firebase.database().ref().update({order:{'fecha':hoy,'orders':aux}});
+            firebase.database().ref().update({order:{'fecha':hoy,'orders':aux}}).then(function(){}).catch(function(error) {
+                setTimeout(function(){
+                    app.sendOrder();
+                },200);
+            });
         }
         else{
-            firebase.database().ref().update({order:{'fecha':hoy,'orders':app.order}});
+            firebase.database().ref().update({order:{'fecha':hoy,'orders':app.order}}).then(function(){}).catch(function(error) {
+                setTimeout(function(){
+                    app.sendOrder();
+                },200);
+            });
         }
     },
 
